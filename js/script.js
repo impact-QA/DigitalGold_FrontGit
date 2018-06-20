c133b3b386380509e42306897a0792c5a3882185
impact-QA <40424376+impact-QA@users.noreply.github.com>
2018-06-20 15:13:17 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mysidenav_resp_dd").style.width = "250px";
+    document.getElementById("bodyMainID").style.position = "relative";
+    document.getElementById("bodyMainID").style.left = "250px";
+}
+
+
+
+
+function closeNav() {
+    document.getElementById("mysidenav_resp_dd").style.width = "0";
+    document.getElementById("bodyMainID").style.left = "0";
+}
+
