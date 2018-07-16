1bb1c3dec37080c5526d13722190fbccef64bb48
impact-QA <40424376+impact-QA@users.noreply.github.com>
2018-07-16 19:09:41 +0530

    Added Redeem

--- js/RedeemDDResp.js
+++ js/RedeemDDResp.js
@@ -17,3 +17,79 @@ $(document).ready(function () {
     }, 10);
 
 });
+
+
+
+$(document).ready(function () {
+    setInterval(function () {
+        if ($(window).width() <= "1199") {
+            $("#dotResp_6").html(". . . . . . . . . .");
+            $("#dotResp_7").html(". . . . . . . . . . . . . . . . . . . .");
+            $("#dotResp_8").html(". . . . . . . . .");
+            $("#dotResp_9").html(". . . . . . . . . . . . . . . . . . ");
+            $("#dotResp_10").html(". . . . . . . . . . . . . . . . . ");
+        }
+        else {
+            $("#dotResp_6").html(". . . . . . . . . . . . . . . .");
+            $("#dotResp_7").html(". . . . . . . . . . . . . . . . . . . . . . . . . . . . ");
+            $("#dotResp_8").html(". . . . . . . . . . . . . . .");
+            $("#dotResp_9").html(". . . . . . . . . . . . . . . . . . . . . . . . . . . ");
+            $("#dotResp_10").html(". . . . . . . . . . . . . . . . . . . . . . . . . . ");
+        }
+    }, 10);
+
+});
+
+
+
+
+
+function red(){
+    window.location.href = "#Addme";
+    if ($(window).width() < 960){
+        $("#sum3").css("display", "block");
+        $("#click").css("display","none"); 
+        $("#sum1").css("display","none");
+        
+        
+    }
+    else{
+        $("#Addme").addClass("bg");
+        $("#Addme").removeClass("colr");
+        $("#addcart").addClass("whiteColor");
+        $("#addcart").removeClass("colr");
+        $("#Addme").css("color","white");
+        $("#sum1").css("display","none");
+        $("#sum2").css("display", "block");
+    }
+}
+
+function confirm2_redir(){
+    if ($(window).width() < 960) {
+        $("#click").css("display","none");
+        $("#confirm1").css("display","none");
+        $("#confirm2").css("display","block");
+            $("#redem_success").removeClass("offset-3");
+           $("#redem_successpara").removeClass("offset-1");
+         
+     }
+     else {
+        $("#confirm1").css("display","none");
+    $("#confirm2").css("display","block");
+     }
+
+}
+
+function gold_redir(){
+    $("#gold_div").css("display","block");
+    $("#silver_div").css("display","none");
+    $("#gold_product_heading").html("Gold Products");
+}
+
+function silver_redir(){
+    $("#gold_div").css("display","none");
+    $("#silver_div").css("display","block");
+    $("#gold_product_heading").html("Silver Products");
+}
+
+ 
