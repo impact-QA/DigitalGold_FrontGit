dae19c8acd1ea28e8d08a378863bf752be36fe22
impact-QA <40424376+impact-QA@users.noreply.github.com>
2018-08-07 17:33:21 +0530

    HomePage + Conv Commit

---
+++ js/home.js
@@ -0,0 +1,59 @@
+
+
+//if ($(window).width() <= "768"){
+//$(".GetStarted").css("background-size","0");
+//$(".GetStarted").css("background","#EEEEEE");
+//$("#container-remove").removeclass("container");
+//}
+
+
+function securityOver() {
+    $("#securityImage").attr("src", "../../img/Security-on-hover.png");
+}
+
+function securityOut() {
+    $("#securityImage").attr("src","../../img/Security.png" );
+}
+
+
+function trustOver() {
+    $("#trustImage").attr("src", "../../img/Trust-on-hover.png");
+}
+
+function trustOut() {
+    $("#trustImage").attr("src", "../../img/Trust.png");
+}
+
+
+function ConvOver() {
+    $("#ConvImage").attr("src", "../../img/convenience-on-hover.png");
+}
+
+function ConvOut() {
+    $("#ConvImage").attr("src", "../../img/convenience.png");
+}
+
+function ProductsOver() {
+    $("#ProductsImage").attr("src", "../../img/selection-on-hover.png");
+}
+
+function ProductsOut() {
+    $("#ProductsImage").attr("src", "../../img/selection.png");
+}
+
+function AccessOver() {
+    $("#AccessImage").attr("src", "../../img/Accessibility-on-hover.png");
+}
+
+function AccessOut() {
+    $("#AccessImage").attr("src", "../../img/Accessibility.png");
+}
+
+function RedemOver() {
+    $("#RedemImage").attr("src", "../../img/Redemption-on-hover.png");
+}
+
+function RedemOut() {
+    $("#RedemImage").attr("src", "../../img/Redemption.png");
+}
+
